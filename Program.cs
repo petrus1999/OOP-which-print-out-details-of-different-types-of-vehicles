84753dad81eac7dc2cda80a35290530227fea72c
petrus <pshai671@gmail.com>
2023-01-26 09:30:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP_which_print_out_details_of_different_types_of_vehicles;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car mycar1 = new Car("POLO GTI","VW","RED");
+        Car mycar2 = new Car("MAZDAX3", "MAZDA", "BLUE");
+        Car mycar3 = new Car(" AMG ", "MERCEDEZ BENZ", "WHITE");
+        Car mycar4 = new Car("MUSTANG", "FORD", "RED");
+
+        Console.WriteLine(mycar1.Name + " " + mycar1.Brand+"  "+ mycar1.Colour);
+        Console.WriteLine(mycar2.Name + " " + mycar2.Brand + " " + mycar2.Colour);
+        Console.WriteLine(mycar3.Name + " " + mycar3.Brand + " " + mycar3.Colour);
+        Console.WriteLine(mycar4.Name + " " + mycar4.Brand + " " + mycar4.Colour);
+
+
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_which_print_out_details_of_different_types_of_vehicles
+{
+     class Vehicle
+    {
+        public string Name;
+        public string Brand;
+        public string Colour;
+        
+        class Car :Vehicle{
+
+            public Car(string name, string brand, string colour)
+            {
+                Name = name;
+                Brand = brand;
+                Colour = colour;
+            }
+        
+        
+        }
+
+    }
+    
+}
